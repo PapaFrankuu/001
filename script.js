04101e513e5db35f3cc30ed6f7fa2a5639473080
Papa Franku <158589681+PapaFrankuu@users.noreply.github.com>
2025-02-16 17:20:34 +0545

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ function greetUser() {
 window.onload = function () {
     greetUser();
 };
+<style>
+  #progress-bar {
+      width: 0;
+      height: 5px;
+      background: #ff3d00;
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 10000;
+      transition: width 0.4s;
+  }
+</style>
+
+<div id="progress-bar"></div>
+
+<script>
+  document.onreadystatechange = function () {
+      let progress = document.getElementById("progress-bar");
+      if (document.readyState === "interactive") {
+          progress.style.width = "50%";
+      } else if (document.readyState === "complete") {
+          progress.style.width = "100%";
+          setTimeout(() => {
+              progress.style.display = "none";
+          }, 500);
+      }
+  };
+</script>
